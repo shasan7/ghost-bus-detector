5461dda9e6a97017872a1f492005eb34be838b85
Md. Intikhab Shahriar Hasan <45317472+shasan7@users.noreply.github.com>
2025-08-11 21:44:32 +0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 import time
 
 from app.fetcher import fetch_gtfs_feed
 from app.parser import parse_vehicle_positions
 
 app = FastAPI()
+
+# Mount React build files under root
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 app.add_middleware(
     CORSMiddleware,
