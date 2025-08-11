d9379401c4703c1920de0a4474f023ef552f0179
Md. Intikhab Shahriar Hasan <45317472+shasan7@users.noreply.github.com>
2025-08-11 21:11:19 +0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["https://ghost-bus-detector.onrender.com"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
